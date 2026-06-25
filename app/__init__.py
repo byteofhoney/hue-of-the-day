1c58ca7b3df8652268936b55f6b439db268f0357
byteofhoney <abeera9sheikh@gmail.com>
2026-06-25 09:34:44 -0700

    feat: connect MongoDB with dotenv config

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from pymongo import MongoClient
+from .config import Config
+
+client = None
+db = None
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+
+    global client, db
+    client = MongoClient(app.config["MONGO_URI"])
+    db = client[app.config["DB_NAME"]]
     
     from .routes import main
     app.register_blueprint(main)
